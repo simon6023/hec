ce0f70324bf380caf8e155b0e5fbaa6ec134380b
Simon Duflos <simon.duflos@gmail.com>
2016-01-27 21:37:54 +0100

    BDD update

--- samples_bdd_insert_variables.php
+++ samples_bdd_insert_variables.php
@@ -2,14 +2,15 @@
 
 	$bdd = new PDO('mysql:host=localhost;dbname=hec_test;charset=utf8', 'root', 'root');
 
-	$requete = $bdd->prepare("INSERT INTO user(firstname, lastname, gender, city, newsletter, created) 
-								VALUES(:firstname, :lastname, :gender, :city, :newsletter, :created)");
+	$requete = $bdd->prepare("INSERT INTO user(firstname, lastname, gender, city, age, newsletter, created) 
+								VALUES(:firstname, :lastname, :gender, :city, :age, :newsletter, :created)");
 
 	$requete->execute(array(
 		'firstname' => $_POST['firstname'],
 		'lastname' => $_POST['lastname'],
 		'gender' => $_POST['gender'],
 		'city' => $_POST['city'],
+		'age' => $_POST['age'],
 		'newsletter' => $_POST['newsletter'],
 		'created' => date('Y-m-d H:i:s'),
 	));
